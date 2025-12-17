572225abe40840dec9aa8254d58458d55f5088ba
Carlos <carlosaac232001@gmail.com>
2025-12-16 22:35:45 -0500

    feat: add name property and create a function to use in onBlur property and render a toast

--- src/components/MultiLineEdit.jsx
+++ src/components/MultiLineEdit.jsx
@@ -1,20 +1,39 @@
 import { useState } from 'react';
+import { toast } from 'sonner';
 
-export default function MultiLineEdit({ value, className }) {
+export default function MultiLineEdit({ name, value, className }) {
   const [editingValue, setEditingValue] = useState(value);
+  const minLength = 40;
 
   function onChange(e) {
     setEditingValue(e.target.value);
   }
 
+  function handleBlur() {
+    if (editingValue.length === 0) {
+      toast.error(`${name} should not be empty.`);
+      return;
+    }
+
+    if (editingValue.length < minLength) {
+      toast.warning(`${name} should be at least ${minLength} characters.`);
+      return;
+    }
+
+    if (value !== editingValue) {
+      toast.success(`${name} successfully saved!`);
+      return;
+    }
+  }
+
   return (
-    <div
-      contentEditable={true}
-      suppressContentEditableWarning={true}
-      dangerouslySetInnerHTML={{ __html: editingValue }}
+    <textarea
+      name={name}
       value={editingValue}
       onChange={onChange}
+      onBlur={handleBlur}
       className={`${className} hover:cursor-pointer hover:bg-neutral-200 focus:outline-none`}
-    ></div>
+      rows={7}
+    ></textarea>
   );
 }
