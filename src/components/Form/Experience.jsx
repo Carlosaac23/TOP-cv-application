a0a1f6e3212c6b8cc341e8ff736eb26d27e219d8
Carlos <carlosaac232001@gmail.com>
2025-12-17 17:47:38 -0500

    feat: create new Experience component

---
+++ src/components/Form/Experience.jsx
@@ -0,0 +1,9 @@
+export default function Experience() {
+  return (
+    <div>
+      <h2 className='mb-2 text-center font-bold tracking-widest uppercase'>
+        Work Experience
+      </h2>
+    </div>
+  );
+}
