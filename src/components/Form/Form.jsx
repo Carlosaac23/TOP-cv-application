6a7936dbc09c7bf716304a935f6ae9176179dc97
Carlos <carlosaac232001@gmail.com>
2025-12-16 21:53:17 -0500

    feat: import new Summary component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,18 @@
 import Contact from './Contact';
 import Name from './Name';
+import Summary from './Summary';
 
 export default function Form() {
   return (
     <main className='m-2 grid grid-cols-2 border-t-2'>
-      <div className='col-span-2 border-b py-8'>
+      <div className='col-span-2 border-b py-6'>
         <Name />
       </div>
-      <div className='col-span-2 border-b py-2'>
+      <div className='col-span-2 border-b py-3'>
         <Contact />
+      </div>
+      <div className='col-span-2 border-b py-2'>
+        <Summary />
       </div>
     </main>
   );
