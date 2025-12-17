ebaa0d50e8c0c1e7be2210cd97365d137a53e6eb
Carlos <carlosaac232001@gmail.com>
2025-12-17 16:05:24 -0500

    style: remove grid style for mobile screen size

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,14 +4,14 @@ import Summary from './Summary';
 
 export default function Form() {
   return (
-    <main className='m-2 grid grid-cols-2 border-t-2'>
-      <div className='col-span-2 border-b py-6'>
+    <main className='m-2 border-t-2'>
+      <div className='border-b py-6'>
         <Name />
       </div>
-      <div className='col-span-2 border-b py-3'>
+      <div className='border-b py-3'>
         <Contact />
       </div>
-      <div className='col-span-2 border-b py-2'>
+      <div className='border-b py-2'>
         <Summary />
       </div>
     </main>
