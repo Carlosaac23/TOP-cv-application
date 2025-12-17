4da49cb81426feafb62d274a0ba5823eafcbdfcb
Carlos <carlosaac232001@gmail.com>
2025-12-16 21:53:33 -0500

    feat: create new Summary component

---
+++ src/components/Form/Summary.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import MultiLineEdit from '../MultiLineEdit';
+
+const sampleText =
+  'Experienced and results-driven Marketing Manager with a proven track record in developing and executing successful marketing strategies. I am seeking a challenging role where I can contribute my skills in strategic planning, team leadership, and creative problem-solving to achieve business objectives.';
+
+export default function Summary() {
+  const [summary, setSummary] = useState(sampleText);
+
+  return (
+    <div className='p-2'>
+      <h2 className='mb-2 text-center font-bold tracking-widest uppercase'>
+        Profile Summary
+      </h2>
+      <MultiLineEdit
+        value={summary}
+        setValue={setSummary}
+        className={'max-h-32 overflow-y-auto text-sm'}
+      />
+    </div>
+  );
+}
