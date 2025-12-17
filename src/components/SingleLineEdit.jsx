75dab5fcb8144455f54646f44646553a6e457b96
Carlos <carlosaac232001@gmail.com>
2025-12-17 16:41:38 -0500

    feat: change destructuring way and use setValue property to update parent state(input)

--- src/components/SingleLineEdit.jsx
+++ src/components/SingleLineEdit.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { toast } from 'sonner';
 
-export default function SingleLineEdit({ name, value, className }) {
+export default function SingleLineEdit(props) {
+  const { name, value, setValue, className } = props;
   const [editingValue, setEditingValue] = useState(value);
 
   function onChange(e) {
@@ -10,6 +11,7 @@ export default function SingleLineEdit({ name, value, className }) {
 
   function handleBlur() {
     if (value !== editingValue) {
+      setValue(editingValue);
       toast.success(`${name} successfully saved!`);
     }
   }
@@ -17,10 +19,10 @@ export default function SingleLineEdit({ name, value, className }) {
   return (
     <input
       type='text'
+      name={name}
       value={editingValue}
       onChange={onChange}
       onBlur={handleBlur}
-      name={name}
       className={`${className} w-full hover:cursor-pointer hover:bg-neutral-200 focus:outline-none`}
     />
   );
