9cb837016153ec0619d7238cbc8cb77df94d9a67
Carlos <carlosaac232001@gmail.com>
2025-12-16 22:33:41 -0500

    feat: - add name property so can render a toast with unique name - create handleBlur function to use it in onBlur property to render a toast

--- src/components/SingleLineEdit.jsx
+++ src/components/SingleLineEdit.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
+import { toast } from 'sonner';
 
-export default function SingleLineEdit({ value, className }) {
+export default function SingleLineEdit({ name, value, className }) {
   const [editingValue, setEditingValue] = useState(value);
 
   function onChange(e) {
     setEditingValue(e.target.value);
   }
 
+  function handleBlur() {
+    if (value !== editingValue) {
+      toast.success(`${name} successfully saved!`);
+    }
+  }
+
   return (
-    <div
-      contentEditable={true}
-      suppressContentEditableWarning={true}
-      dangerouslySetInnerHTML={{ __html: editingValue }}
+    <input
+      type='text'
       value={editingValue}
       onChange={onChange}
+      onBlur={handleBlur}
+      name={name}
       className={`${className} w-full hover:cursor-pointer hover:bg-neutral-200 focus:outline-none`}
-    ></div>
+    />
   );
 }
