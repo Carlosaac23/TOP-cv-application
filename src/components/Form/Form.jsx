f1474136d73d4ea007fc42fc14f73bfb5dd306ea
Carlos <carlosaac232001@gmail.com>
2025-12-18 15:35:35 -0500

    style: remove all outer divs of each component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,21 +9,11 @@ export default function Form() {
   return (
     <main className='m-2 border-t-2'>
       <Name />
-      <div className='border-b py-3'>
-        <Contact />
-      </div>
-      <div className='border-b py-2'>
-        <Summary />
-      </div>
-      <div className='border-b py-2'>
-        <Education />
-      </div>
-      <div className='border-b py-2'>
-        <Experience />
-      </div>
-      <div className='border-b py-2'>
-        <Skills />
-      </div>
+      <Contact />
+      <Summary />
+      <Education />
+      <Experience />
+      <Skills />
     </main>
   );
 }
