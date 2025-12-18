589dc6826c54c6d2f4f41a93a5a454b5c449051e
Carlos <carlosaac232001@gmail.com>
2025-12-18 16:41:17 -0500

    feat: create new Skills component

--- src/components/Form/Skills.jsx
+++ src/components/Form/Skills.jsx
@@ -1,3 +1,42 @@
+import { useState } from 'react';
+import SingleLineEdit from '../SingleLineEdit';
+
 export default function Skills() {
-  return <p>Probando</p>;
+  const [skills, setSkills] = useState([
+    'Project Management',
+    'Public Relations',
+    'Teamwork',
+    'Time Management',
+    'Leadership',
+    'Effective Communication',
+    'Critical Thinking',
+  ]);
+
+  return (
+    <div className='border-b px-2 py-4'>
+      <h2 className='mb-2 text-center font-bold tracking-widest uppercase'>
+        Skills
+      </h2>
+
+      {skills.map((skill, index) => {
+        function updateSkill(newValue) {
+          const newSkills = [...skills];
+          newSkills[index] = newValue;
+          setSkills(newSkills);
+        }
+
+        return (
+          <div key={index} className='flex items-baseline gap-2'>
+            <span className='before:content-["•"]'></span>
+            <SingleLineEdit
+              name='Skill'
+              value={skill}
+              setValue={updateSkill}
+              className={'text-sm'}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
