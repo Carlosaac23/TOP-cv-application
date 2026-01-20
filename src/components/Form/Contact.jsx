28317ffb4235aa68c112f725cf631cd9f58fbf71
Carlos <carlosaac232001@gmail.com>
2026-01-20 18:44:59 -0500

    feat: import useContact custom hook

--- src/components/Form/Contact.jsx
+++ src/components/Form/Contact.jsx
@@ -1,40 +1,8 @@
-import { Earth, Mail, MapPin, Phone } from 'lucide-react';
-import { useState } from 'react';
-
 import SingleLineEdit from '@/components/SingleLineEdit';
+import { useContact } from '@/hooks/useContact';
 
 export default function Contact() {
-  const [telephone, setTelephone] = useState('+57 300-184-1584');
-  const [email, setEmail] = useState('johndoe@gmail.com');
-  const [address, setAddress] = useState('123 Anywhere St., Any City');
-  const [website, setWebsite] = useState('www.johndoewebsite.com');
-
-  const contactMethods = [
-    {
-      name: 'Contact Phone',
-      icon: <Phone size={16} color='#404040' />,
-      value: telephone,
-      setValue: setTelephone,
-    },
-    {
-      name: 'Email',
-      icon: <Mail size={16} color='#404040' />,
-      value: email,
-      setValue: setEmail,
-    },
-    {
-      name: 'Location',
-      icon: <MapPin size={16} color='#404040' />,
-      value: address,
-      setValue: setAddress,
-    },
-    {
-      name: 'Website',
-      icon: <Earth size={16} color='#404040' />,
-      value: website,
-      setValue: setWebsite,
-    },
-  ];
+  const { contactMethods } = useContact();
 
   return (
     <div className='flex flex-col gap-1 border-b px-2 py-4 md:border-r md:px-4'>
