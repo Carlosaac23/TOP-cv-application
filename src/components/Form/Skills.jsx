95c3237cb52449209e450ce81b160a30ad970fe4
Carlos <carlosaac232001@gmail.com>
2026-01-20 20:00:55 -0500

    feat: create useSkills custom hook to manage all skills data

--- src/components/Form/Skills.jsx
+++ src/components/Form/Skills.jsx
@@ -1,39 +1,23 @@
-import { useState } from 'react';
-
 import SingleLineEdit from '@/components/SingleLineEdit';
+import { useSkills } from '@/hooks/useSkills';
 
 export default function Skills() {
-  const [skills, setSkills] = useState([
-    'Project Management',
-    'Public Relations',
-    'Teamwork',
-    'Time Management',
-    'Leadership',
-    'Effective Communication',
-    'Critical Thinking',
-  ]);
+  const { skills, updateSkill } = useSkills();
 
   return (
     <div className='border-b px-2 py-4 md:border-r md:px-4'>
       <h2 className='mb-2 text-center font-bold tracking-widest uppercase md:text-left md:text-xl'>
         Skills
       </h2>
-
       {skills.map((skill, index) => {
-        function updateSkill(newValue) {
-          const newSkills = [...skills];
-          newSkills[index] = newValue;
-          setSkills(newSkills);
-        }
-
         return (
           <div key={index} className='flex items-baseline gap-2'>
             <span className='before:content-["•"]'></span>
             <SingleLineEdit
               name='Skill'
               value={skill}
-              setValue={updateSkill}
-              className={'text-sm'}
+              setValue={newValue => updateSkill(index, newValue)}
+              className='text-sm'
             />
           </div>
         );
