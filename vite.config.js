1ab02e766ef1603b6ecd545bfa46c8d80c0bc21b
Carlos <carlosaac232001@gmail.com>
2026-01-20 19:39:34 -0500

    feat: add base config for github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/TOP-cv-application/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
